0298dffc652f68c66f2b8c42289115f1cda88f02
BrunoSong <brunosong@wjtb.net>
2024-05-21 08:50:51 +0900

    도커 컴포즈로 셋팅

--- src/main/java/com/study/userservice/client/OrderServiceClient.java
+++ src/main/java/com/study/userservice/client/OrderServiceClient.java
@@ -2,7 +2,6 @@ package com.study.userservice.client;
 
 import com.study.userservice.vo.ResponseOrder;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
